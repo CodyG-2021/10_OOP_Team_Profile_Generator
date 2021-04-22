762c2f662289d2359c5f74450f39ee4b0b8d8f52
CodyG <gallagher2021@gmail.com>
2021-04-22 15:57:42 -0600

    Updated index file to add validation to the questions

--- develop/index.js
+++ develop/index.js
@@ -12,22 +12,54 @@ const questions = [
 	{
 		type: 'input',
 		name: 'managerName',
-		message: 'What is the managers name?'
+		message: 'What is the managers name?',
+		validate: input => {
+			if (input) {
+					return true;
+			} else {
+					console.log("Please enter the managers to continue.");
+					return false;
+			}
+	  }
 	},
 	{
 		type: 'input',
 		name: 'managerId',
-		message: 'What is the managers employee ID?'
+		message: 'What is the managers employee ID?',
+		validate: input => {
+			if (input) {
+					return true;
+			} else {
+					console.log("Please enter the managers ID to continue.");
+					return false;
+			}
+	  }
 	},
 	{
 		type: 'input',
 		name: 'managerEmail',
-		message: 'What is the managers E-Mail?'
+		message: 'What is the managers E-Mail?',
+		validate: input => {
+			if (input) {
+					return true;
+			} else {
+					console.log("Please enter the managers e-mail to continue.");
+					return false;
+			}
+	  }
 	},
 	{
 		type: 'input',
 		name: 'managerOfficeNum',
-		message: 'What is the managers office number?'
+		message: 'What is the managers office number?',
+		validate: input => {
+			if (input) {
+					return true;
+			} else {
+					console.log("Please enter the managers office number to continue.");
+					return false;
+			}
+	  }
 	}
 ];
 
