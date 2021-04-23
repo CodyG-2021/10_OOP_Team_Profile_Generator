9f3c489aebd2729855afc0fc9e64a8fd56c6366b
CodyG <gallagher2021@gmail.com>
2021-04-23 10:38:09 -0600

    Made seperate questions file to import to index, may not work

--- develop/index.js
+++ develop/index.js
@@ -1,173 +1,56 @@
 //Pull in file system and utilize inquirer package
-const { createEmptyTestResult } = require('@jest/test-result');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { createEmptyTestResult } = require('@jest/test-result');
 const { inherits } = require('node:util');
 const { choices } = require('yargs');
-//Pull in library 
+//Pulls in library 
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+//Pulls in questions as to not clutter index file
+import {managerQuest, addEmpQuest, addEmpType, engineerQuest, internQuest} from './lib/questions';
 
 //Holder array for added members
 const team = [];
 
-//Initial manager questions
-const managerQuest = [
-	{
-		type: 'input',
-		name: 'managerName',
-		message: 'What is the managers name?',
-		validate: input => {
-			if (input) {
-					return true;
-			} else {
-					console.log("Please enter the managers to continue.");
-					return false;
-			}
-	  }
-	},
-	{
-		type: 'input',
-		name: 'managerId',
-		message: 'What is the managers employee ID?',
-		validate: input => {
-			if (input) {
-					return true;
-			} else {
-					console.log("Please enter the managers ID to continue.");
-					return false;
-			}
-	  }
-	},
-	{
-		type: 'input',
-		name: 'managerEmail',
-		message: 'What is the managers E-Mail?',
-		validate: input => {
-			if (input) {
-					return true;
-			} else {
-					console.log("Please enter the managers e-mail to continue.");
-					return false;
-			}
-	  }
-	},
-	{
-		type: 'input',
-		name: 'managerOfficeNum',
-		message: 'What is the managers office number?',
-		validate: input => {
-			if (input) {
-					return true;
-			} else {
-					console.log("Please enter the managers office number to continue.");
-					return false;
-			}
-	  }
-	}
-];
-
-const addEmpQuest = [
-	{
-		type: 'list',
-		name: 'empAdd',
-		message: 'Would you like to add more employees?',
-		choices: ['Yes', 'No']
-	}
-]
-
-const addEmpType = [
-	{
-		type: 'list',
-		name: 'typeEmp',
-		message: 'What employee position would you like to add?',
-		choices: ['Engineer', 'Intern']
-	}
-]
-
-const engineerQuest = [
-	{
-		type: 'input',
-		name: 'engName',
-		message: 'What is the Engineer\'s name?'
-	},
-	{
-		type: 'input',
-		name: 'engId',
-		message: 'What is the Engineer\'s ID Number?'
-	},
-	{
-		type: 'input',
-		name: 'engEmail',
-		message: 'What is the Engineer\'s E-Mail?'
-	},
-	{
-		type: 'input',
-		name: 'engGithub',
-		message: 'What is the Engineer\'s GitHub account?'
-	}
-]
-const internQuest = [
-	{
-		type: 'input',
-		name: 'intName',
-		message: 'What is the Intern\'s name?'
-	},
-	{
-		type: 'input',
-		name: 'intId',
-		message: 'What is the Intern\'s ID Number?'
-	},
-	{
-		type: 'input',
-		name: 'intEmail',
-		message: 'What is the Intern\'s E-Mail?'
-	},
-	{
-		type: 'input',
-		name: 'intSchool',
-		message: 'What school does the Intern go to?'
-	}
-]
-
 function init() {
 	inquirer
-	.prompt(managerQuest)
-	.then(res => {
-		const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOfficeNum);
-		team.push(manager);
-		addEmployees();
-	})
+		.prompt(managerQuest)
+		.then(res => {
+			const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOfficeNum);
+			team.push(manager);
+			addEmployees();
+		})
 	// .then(res => {
 	// 	writeToFile('./dummyOutput/myTeam.html', generateHTML(res));
 	// });
 };
 
-function addEmployees () {
+function addEmployees() {
 	inquirer
-	.prompt(addEmpQuest)
-	.then (res => {
-		res = 'Yes'
-		? createEmp()
-		: writeToFile()
-	});
+		.prompt(addEmpQuest)
+		.then(res => {
+			res = 'Yes'
+				? createEmp()
+				: writeToFile()
+		});
 };
 
 function createEmp() {
 	inquirer
-	.prompt(addEmpType)
-	.then(res => {
-		if (res === 'Engineer') {
-			const engineer = new Engineer (res.engName, res.engId, res.engEmail, res.engGithub);
-			team.push(engineer);
-			addEmployees();
-		}
-			const intern = new Intern (res.intName, res.intId, res.intEmail, res.intSchool);
+		.prompt(addEmpType)
+		.then(res => {
+			if (res === 'Engineer') {
+				const engineer = new Engineer(res.engName, res.engId, res.engEmail, res.engGithub);
+				team.push(engineer);
+				addEmployees();
+			}
+			const intern = new Intern(res.intName, res.intId, res.intEmail, res.intSchool);
 			team.push(intern);
 			addEmployees();
-	})
+		})
 }
 
 // function writeToFile(fileName, f) {
